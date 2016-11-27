993bfcfae0792fa567cbb8a6a2cfe8d61100140d
MeriKhachikyan <meri_khachikyan@edu.aua.am>
2016-11-27 20:36:50 +0400

    Add files via upload

---
+++ driver.java
@@ -0,0 +1,15 @@
+import Rectangle;
+import Square;
+import Math;
+public class Driver {
+    public static void main(String[] args){
+        Square mySquare = new Square(100);
+        System.out.println(mySquare.getArea());
+        Rectangle myRectangle = new Rectangle(50, 80);
+        System.out.println(myRectangle.getArea());
+        double number = 15.6;
+        int parsedNumber = (int) number;
+        System.out.println(Math.factorial(parsedNumber));
+        System.out.println(Math.factorialLoop(parsedNumber));
+    }
+}
